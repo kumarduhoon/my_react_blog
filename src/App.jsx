705915b4a47507579315b19fa38483e8aa1fb42a
Akash Kumar <akash.kumar@arkasoftwares.com>
2024-03-02 11:39:00 +0530

    redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import React from 'react'
 import './App.css'
+import authService from './appwrite/auth'
+import { useDispatch } from 'react-redux'
+import { login, logout } from './redux/reducer/authSlice'
+import { Header, Footer } from './component'
+import { Outlet } from 'react-router-dom'
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">test</h1>
+  const [loading, setLoading] = React.useState(true)
+  const dispatch = useDispatch()
+  React.useState(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData))
+        }
+        else {
+          dispatch(logout())
+        }
+      }).catch((error) => {
+        throw error
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+  if (!loading) {
+    return <>
+      <Header />
+      <Outlet />
+      <Footer />
     </>
-  )
+  }
 }
 
 export default App
